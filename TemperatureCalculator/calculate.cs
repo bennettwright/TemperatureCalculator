703bbe621d6f9fa906a5cb42b5a8e1e2e048399b
bennett-wright <bennett.wright3@gmail.com>
2018-04-23 16:22:53 -0700

    Initial Commit

--- TemperatureCalculator/calculate.cs
+++ TemperatureCalculator/calculate.cs
@@ -8,28 +8,31 @@ namespace TemperatureCalculator
         {
         }
 
-        private static decimal fahrenheit, humidity, windSpeed;
+        private static double fahrenheit, humidity;
 
-        public static decimal getTemp(string fahrenheit, int windSpeed)
+        //equation variables
+        const double E1 = 35.74, E2 = 0.6215, E3 = 35.75, E4 = 0.4275, E5 = 0.16;
+
+        public static double getTemp(string fDegrees, int windSpeed)
         {
-            if (validator(fahrenheit))
-                return getWindChill();
-            
-
+            if (validator(fDegrees))
+                return E1 + (E2 * fahrenheit) - Math.Pow(E3 * windSpeed, E5) 
+                                                    + (E4 * fahrenheit) * Math.Pow(windSpeed, E5);
+            throw new ArgumentException("Not valid input");
         }
 
-        public static decimal getTemp(string fehrenheit, string humidity, int windSpeed)
+        public static double getTemp(string fehrenheit, string humidity, int windSpeed)
         {
             
         }
 
 
         //making humid null by default because we don't need humidity
-        //every time in order to get the temp....and don't want to be passing null
+        //in order to get the temp....and don't want to be passing null
         //in the params every time
         private static bool validator(string fDegrees, string humid = null) =>
-            (humid == null) ? Decimal.TryParse(fDegrees, out fahrenheit) :
-                                    Decimal.TryParse(fDegrees, out fahrenheit) &&
-                                            Decimal.TryParse(humid, out humidity);
+            (humid == null) ? Double.TryParse(fDegrees, out fahrenheit) :
+                                    Double.TryParse(fDegrees, out fahrenheit) &&
+                                            Double.TryParse(humid, out humidity);
     }
 }
